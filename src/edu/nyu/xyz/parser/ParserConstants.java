12c3480c2969e6db00e053faf6368fdb42cdead5
Long Yang <jasonyl0806@yahoo.com>
2014-04-17 15:50:27 -0400

    XYZ RTBD project commit: 1. Finish the glassdoor data parser work.

---
+++ src/edu/nyu/xyz/parser/ParserConstants.java
@@ -0,0 +1,18 @@
+package edu.nyu.xyz.parser;
+
+public class ParserConstants {
+	// 1. GlassDoor constants
+	// 1.1. Constants for output file
+	public static String POSITION = "position";
+	public static String POSITION_RANGE = "position-range";
+	public static String POSITION_NUMBER = "position-number";
+	public static String NAME = "name";
+	
+	// 1.2. Constants for input file
+	public static String INFO = "info";
+	public static String META = "meta";
+	public static String INDUSTRY = "Industry";
+	public static String SALARY = "salary";
+	public static String RANGE = "range";
+	public static String ALL = "All";
+}
